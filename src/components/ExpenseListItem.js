ab07b86c43ce44972209e122f821f85a2cfaee8d
Roy <rs58@rice.edu>
2018-01-02 18:28:43 -0500

    used numeral library to format costs

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt, browserHistory }) => (
   <div>
     <h3>{description}</h3>
-    <p>Amount: {amount}</p>
-    <p>Created: {createdAt}</p>
+    <p>Amount: {numeral(amount / 100).format('$0,0.00')}</p>
+    <p>Created: {moment(createdAt).format('MMMM Do, YYYY')}</p>
     <Link to={"/edit/" + id}><button>Edit</button></Link>
   </div>
 );
