8d185ea67a5d7e0640dc8cfde8f128fd17997f43
Roy <rs58@rice.edu>
2018-01-17 17:24:04 -0600

    added hiding expense subtitle to dashboard

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -7,13 +7,16 @@ import numeral from 'numeral';
 
 export const ExpensesSummary = (props) => {
   const expensesCount = props.expenses.length;
+  const hiddenExpensesCount = props.allExpenses.length - expensesCount;
   const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
+  const hiddenExpenseWord = hiddenExpensesCount === 1 ? 'expense' : 'expenses';
   const expensesSum = numeral(selectExpensesTotal(props.expenses) / 100).format('$0,0.00');
 
   return (
     <div className="page-header">
       <div className="content-container">
         <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{expensesSum}</span></h1>
+        <h3 className="page-header__subtitle">Hiding <span>{hiddenExpensesCount}</span> {hiddenExpenseWord}</h3>
         <div className="page-header__actions">
           <Link className="button" to="/create">Add Expense</Link>
         </div>
@@ -23,7 +26,8 @@ export const ExpensesSummary = (props) => {
 };
 
 const mapStateToProps = (state) => ({
-  expenses: selectExpenses(state.expenses, state.filters)
+  expenses: selectExpenses(state.expenses, state.filters),
+  allExpenses: state.expenses
 });
 
 export default connect(mapStateToProps)(ExpensesSummary);
