c823b749a59476a489d0741846debca61b93dcb3
Roy <rs58@rice.edu>
2018-01-29 17:39:05 -0600

    updated test cases for summary page

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -8,7 +8,19 @@ import thunk from 'redux-thunk';
 test('should render ExpenseListItem with fixture data', () => {
   const createMockStore = configureMockStore([thunk]);
   const store = createMockStore();
-  const wrapper = shallow(<ExpenseListItem {...expenses[0]} store={store} />);
+  const wrapper = shallow(<ExpenseListItem {...expenses[0]} allExpenses={expenses} store={store} />);
   expect(wrapper).toMatchSnapshot();
 });
 
+test('should call the delete button correctly', () => {
+  const onClick = jest.fn();
+  const startRemoveButton = jest.fn();
+  const preventDefault = jest.fn();
+  const wrapper = shallow(<ExpenseListItem onClick={onClick} startRemoveButton={startRemoveButton} />);
+  wrapper.find('button').simulate('click', {
+    preventDefault: () => {
+
+    }
+  });
+  expect(onClick).toHaveBeenLastCalledWith();
+});
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -4,11 +4,11 @@ import { ExpensesSummary } from '../../components/ExpensesSummary';
 import expenses from '../fixtures/expenses';
 
 test('should render a single expense correctly', () => {
-  const wrapper = shallow(<ExpensesSummary expenses={[expenses[0]]} />);
+  const wrapper = shallow(<ExpensesSummary expenses={[expenses[0]]} allExpenses={expenses}/>);
   expect(wrapper).toMatchSnapshot();
 });
 
 test('should render multiple expenses correctly', () => {
-  const wrapper = shallow(<ExpensesSummary expenses={expenses} />);
+  const wrapper = shallow(<ExpensesSummary expenses={expenses} allExpenses={expenses}/>);
   expect(wrapper).toMatchSnapshot();
 });
